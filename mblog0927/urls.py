6480b9190da500c9aac5a4d385a3a8e4a8d1cb28
weiyuchen0820 <10956036@ntub.edu.tw>
2023-10-11 14:17:22 +0800

    10/11 02:17

--- mblog0927/urls.py
+++ mblog0927/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mysite.views import views as mv
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', mv.homepage, name="homepage")
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from mysite.models import Post
+from django.http import HttpResponse
 
 # Create your views here.
+def homepage(request):
+    posts = Post.objects.all() #select from post
+    post_lists = list()
+    for counter,post in enumerate(posts):
+        post_lists.append(f'No. {counter} {post} <br>')
+    return HttpResponse(post_lists)

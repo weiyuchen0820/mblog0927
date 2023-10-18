4139ce4653447334cf7dbb4e8233032ed855eb86
weiyuchen0820 <10956036@ntub.edu.tw>
2023-10-18 16:15:27 +0800

    到簡報Ch2 P69 老師會上傳程式碼到他的git

--- mysite/views.py
+++ mysite/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from mysite.models import Post
 from django.http import HttpResponse
 from datetime import datetime
+from django.shortcuts import redirect
 
 # Create your views here.
 def homepage(request):
@@ -10,8 +11,14 @@ def homepage(request):
     return render (request, 'index.html', locals())
 
 def showpost(request,slug):
-    post = Post.objects.get(slug=slug)
-    return render(request, 'post.html', locals())
+    try:
+        post = Post.objects.get(slug=slug)
+        if post != None:
+            return render(request, 'post.html', locals())
+        else:
+            return redirect("/")
+    except:
+        return redirect("/")
 
 '''
 def homepage(request):

ffb3f0064fd18604024363a97ee1da6cbbcc9d54
weiyuchen0820 <10956036@ntub.edu.tw>
2023-11-15 15:51:54 +0800

    另個葉面

--- mysite/views.py
+++ mysite/views.py
@@ -13,6 +13,31 @@ def homepage(request):
 def showpost(request,slug):
     post = Post.objects.get(slug=slug)
     return render(request, 'post.html', locals())
+
+def about(request):
+    mhtml = '''
+<html>
+<body>
+<h1>I</h1>
+<h3>am in NTUB</h3>
+<h2>{num}</h2>
+</body></html>
+'''
+    return HttpResponse(mhtml)
+
+import random
+def about(request, num=-1):
+        quotes = ['今日事，今日畢',
+        '要怎麼收穫，先那麼栽',
+        '知識就是力量',
+        '一個人的個性就是他的命運']
+        if num == -1 or num > 4:
+            quote = random.choice(quotes)
+        else:
+             quote = quotes[num]
+        return render(request, 'about.html', locals())   
+
+
 '''
 def homepage(request):
     posts = Post.objects.all() #select from post
